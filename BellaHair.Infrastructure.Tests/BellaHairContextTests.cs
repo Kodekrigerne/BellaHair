f7ed6932fb9cab3e16ddaff0e7694d4471ad8749
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-10 10:48:09 +0100

    Added test to ensure DbContext can build.

---
+++ BellaHair.Infrastructure.Tests/BellaHairContextTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BellaHair.Infrastructure.Tests
+{
+    internal class BellaHairContextTests
+    {
+        [Test]
+        public void CanConstruct()
+        {
+            var options = new DbContextOptionsBuilder<BellaHairContext>()
+                .UseSqlite("Data Source=:memory:")
+                .Options;
+
+            Assert.DoesNotThrow(() => new BellaHairContext(options));
+        }
+    }
+}
