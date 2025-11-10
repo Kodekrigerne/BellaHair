bbd221682d604eeb8400a61a69241f79dba716d4
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-10 10:47:09 +0100

    Added a DbContext to Infrastructure and added DependencyInjection extensions to Infrastructure and Application.

---
+++ BellaHair.Infrastructure/BellaHairContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BellaHair.Infrastructure
+{
+    public class BellaHairContext : DbContext
+    {
+        public BellaHairContext(DbContextOptions<BellaHairContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}
---
+++ BellaHair.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BellaHair.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<BellaHairContext>();
+
+            return serviceCollection;
+        }
+    }
+}
