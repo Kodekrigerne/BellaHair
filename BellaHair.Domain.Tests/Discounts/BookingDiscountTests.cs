edce6a9caa4540d8fa3965a8c522e7e648ff462d
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-10 17:37:39 +0100

    Created a BookingDiscount value object and tests for it.

---
+++ BellaHair.Domain.Tests/Discounts/BookingDiscountTests.cs
@@ -0,0 +1,38 @@
+﻿using BellaHair.Domain.Discounts;
+
+namespace BellaHair.Domain.Tests.Discounts
+{
+    //Dennis
+    internal sealed class BookingDiscountTests
+    {
+        [Test]
+        public void Given_ActiveFactory_Then_ConstructsActiveBookingDiscount()
+        {
+            var name = "Test Name";
+            var discountAmount = 5.25m;
+
+            var bookingDiscount = BookingDiscount.Active(name, discountAmount);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(bookingDiscount.Name, Is.EqualTo(name));
+                Assert.That(bookingDiscount.Amount, Is.EqualTo(discountAmount));
+                Assert.That(bookingDiscount.DiscountActive, Is.True);
+            });
+        }
+
+        [Test]
+        public void Given_InActiveFactory_Then_ConstructsInActiveBookingDiscount()
+        {
+            var name = "Test Name";
+
+            var bookingDiscount = BookingDiscount.Inactive(name);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(bookingDiscount.Name, Is.EqualTo(name));
+                Assert.That(bookingDiscount.DiscountActive, Is.False);
+            });
+        }
+    }
+}
