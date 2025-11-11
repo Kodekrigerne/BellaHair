997e578e8fee665911d628d067cc6e367d10e727
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-11 12:38:13 +0100

    Refactor Address validation and add unit tests
    Refined validation logic in the Address class:
    - Allowed hyphens in street and city names.
    - Simplified conditions for invalid characters.
    - Moved AddressException to a single-line declaration.
    
    Added AddressTests.cs with comprehensive unit tests:
    - Validated address creation with/without floor values.
    - Tested handling of whitespace in street and city names.
    - Verified FullAddress string generation.
    - Ensured invalid inputs throw AddressException.

---
+++ BellaHair.Domain.Tests/SharedValueObjects/AddressTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Domain.SharedValueObjects;
+
+namespace BellaHair.Domain.Tests.SharedValueObjects
+{
+    internal sealed class AddressTests
+    {
+        [Test]
+        public void Given_ValidAddressWithFloor_Then_CreatesAddress()
+        {
+            // Arrange
+            var streetName = "Nygade";
+            var city = "Vejle";
+            var streetNumber = "10";
+            var floor = 2;
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode, floor);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(testAddress.StreetName, Is.EqualTo(streetName));
+                Assert.That(testAddress.City, Is.EqualTo(city));
+                Assert.That(testAddress.StreetNumber, Is.EqualTo(streetNumber));
+                Assert.That(testAddress.Floor, Is.EqualTo(floor));
+                Assert.That(testAddress.ZipCode, Is.EqualTo(zipCode));
+            });
+        }
+
+        [Test]
+        public void Given_ValidAddressWithoutFloor_Then_CreatesAddressWithNullFloorValue()
+        {
+            // Arrange
+            var streetName = "Nygade";
+            var city = "Vejle";
+            var streetNumber = "10";
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(testAddress.StreetName, Is.EqualTo(streetName));
+                Assert.That(testAddress.City, Is.EqualTo(city));
+                Assert.That(testAddress.StreetNumber, Is.EqualTo(streetNumber));
+                Assert.That(testAddress.Floor, Is.EqualTo(null));
+                Assert.That(testAddress.ZipCode, Is.EqualTo(zipCode));
+            });
+        }
+
+        [Test]
+        public void Given_ValidAddressWithWhitespaceCity_Then_CreatesAddress()
+        {
+            // Arrange
+            var streetName = "Nygade";
+            var city = "Nørre Aaby";
+            var streetNumber = "10";
+            var floor = 2;
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode, floor);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(testAddress.StreetName, Is.EqualTo(streetName));
+                Assert.That(testAddress.City, Is.EqualTo(city));
+                Assert.That(testAddress.StreetNumber, Is.EqualTo(streetNumber));
+                Assert.That(testAddress.Floor, Is.EqualTo(floor));
+                Assert.That(testAddress.ZipCode, Is.EqualTo(zipCode));
+            });
+        }
+
+        [Test]
+        public void Given_ValidAddressWithWhitespaceStreetName_Then_CreatesAddress()
+        {
+            // Arrange
+            var streetName = "Ny Banegade";
+            var city = "Vejle";
+            var streetNumber = "10";
+            var floor = 2;
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode, floor);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(testAddress.StreetName, Is.EqualTo(streetName));
+                Assert.That(testAddress.City, Is.EqualTo(city));
+                Assert.That(testAddress.StreetNumber, Is.EqualTo(streetNumber));
+                Assert.That(testAddress.Floor, Is.EqualTo(floor));
+                Assert.That(testAddress.ZipCode, Is.EqualTo(zipCode));
+            });
+        }
+
+        [Test]
+        public void Given_ValidAddressWithFloor_Then_CreatesCorrectFullAddressString()
+        {
+            // Arrange
+            var streetName = "Nygade";
+            var city = "Vejle";
+            var streetNumber = "10";
+            var floor = 2;
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode, floor);
+
+            // Assert
+            Assert.That(testAddress.FullAddress, Is.EqualTo($"{streetName} {streetNumber}, {floor}. sal, {zipCode} {city}"));
+        }
+
+        [Test]
+        public void Given_ValidAddressWithoutFloor_Then_CreatesCorrectFullAddressString()
+        {
+            // Arrange
+            var streetName = "Nygade";
+            var city = "Vejle";
+            var streetNumber = "10";
+            var zipCode = 7100;
+
+            // Act
+            var testAddress = Address.FromInputs(streetName, city, streetNumber, zipCode);
+
+            // Assert
+            Assert.That(testAddress.FullAddress, Is.EqualTo($"{streetName} {streetNumber}, {zipCode} {city}"));
+        }
+
+        [TestCase("Nygade5", "Vejle", "10", 2, 7100)]
+        [TestCase("Nygade", "Vejle5", "10", 2, 7100)]
+        [TestCase("Nygade", "Vejle", "10-", 2, 7100)]
+        [TestCase("Nygade", "Vejle", "10", -2, 7100)]
+        [TestCase("Nygade", "Vejle", "10", 2, 71005)]
+        public void Given_InvalidParameter_Then_ThrowsAddressException(string streetName, string city,
+            string streetNumber, int floor, int zipCode)
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<AddressException>(() => Address.FromInputs(streetName, city, streetNumber, zipCode, floor));
+        }
+
+    }
+}
