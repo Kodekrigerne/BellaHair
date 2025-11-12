f3666f985f4a0b2e57119190a867b548f0a3fa40
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-12 13:54:59 +0100

    Add InfrastructureTestBase for database test setup
    Introduced a new abstract class `InfrastructureTestBase` to
    streamline database setup and teardown for infrastructure
    tests. The class uses an SQLite database with
    Entity Framework Core to ensure a clean state for each test
    run. Added `OneTimeSetUp`, `SetUp`, and `OneTimeTearDown`
    methods for lifecycle management.

---
+++ BellaHair.Infrastructure.Tests/InfrastructureTestBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BellaHair.Infrastructure.Tests
+{
+    public abstract class InfrastructureTestBase
+    {
+        private DbContextOptions<BellaHairContext> _options;
+        private BellaHairContext _db;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _options = new DbContextOptionsBuilder<BellaHairContext>().UseSqlite("Data Source=:memory:").Options;
+            _db = new BellaHairContext(_options);
+            _db.Database.OpenConnection();
+            _db.Database.EnsureCreated();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _db.Database.EnsureDeleted();
+            _db.Database.EnsureCreated();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _db.Dispose();
+        }
+    }
+}
