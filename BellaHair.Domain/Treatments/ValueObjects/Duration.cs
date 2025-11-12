0c9b7ed08cdcdffd1460638b68bb368226d0ec26
mikl0w <mikkelklitgaard@live.dk>
2025-11-12 14:00:24 +0100

    Resolved

--- BellaHair.Domain/Treatments/ValueObjects/Duration.cs
+++ BellaHair.Domain/Treatments/ValueObjects/Duration.cs
@@ -1,13 +1,17 @@
-﻿namespace BellaHair.Domain.Treatments.Value_Objects
+﻿namespace BellaHair.Domain.Treatments.ValueObjects
 {
     //Mikkel Klitgaard
+
+    /// <summary>
+    /// Represents a duration value withing a valid range (between 10 minutes and 5 hours).
+    /// Instances of this type are immutable and can only be created using the <see cref="FromInt(int)"/> method,
+    /// which validates the input.
+    /// </summary>
     public record Duration
     {
         public int Value { get; private init; }
 
-
         protected Duration() { }
-
 
         private Duration(int value)
         {
@@ -17,7 +21,6 @@
 
         public static Duration FromInt(int duration) => new Duration(duration);
 
-
         public static void ValidateDuration(int value)
         {
             if (value < 10)
@@ -25,9 +28,6 @@
             if (value > 300)
                 throw new DurationException("The duration must be less than 5 hours.");
         }
-
-
     }
-
     public class DurationException(string message) : DomainException(message);
 }
