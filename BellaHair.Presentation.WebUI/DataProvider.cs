e501a16fc578fcf7e2a193d32ac8712d80cea209
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-12 20:58:57 +0100

    Added a DataProvider class which injects test data into the app DbContext.

---
+++ BellaHair.Presentation.WebUI/DataProvider.cs
@@ -0,0 +1,24 @@
+﻿using BellaHair.Domain.Discounts;
+using BellaHair.Infrastructure;
+
+namespace BellaHair.Presentation.WebUI
+{
+    public class DataProvider
+    {
+        private readonly BellaHairContext _db;
+
+        public DataProvider(BellaHairContext db) => _db = db;
+
+        public void AddData()
+        {
+            AddLoyaltyDiscounts();
+        }
+
+        private void AddLoyaltyDiscounts()
+        {
+            _db.Add(LoyaltyDiscount.Create("Loyalty5", 5, DiscountPercent.FromDecimal(5)));
+            _db.Add(LoyaltyDiscount.Create("Loyalty5", 10, DiscountPercent.FromDecimal(10)));
+            _db.Add(LoyaltyDiscount.Create("Loyalty5", 15, DiscountPercent.FromDecimal(15)));
+        }
+    }
+}
