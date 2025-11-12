5f52a6d0f6fdf8393953d78de4db825b19ba2d65
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-12 12:23:39 +0100

    Add private customer command and query interfaces
    Introduced `IPrivateCustomerCommand` and `IPrivateCustomerQuery` interfaces to define contracts for managing and retrieving private customer entities.
    
    - Added `CreatePrivateCustomerCommand`, `UpdatePrivateCustomerCommand`, and `DeletePrivateCustomerCommand` for command operations.
    - Added `PrivateCustomerDTO` for query operations.

---
+++ BellaHair.Ports/PrivateCustomers/IPrivateCustomerCommand.cs
@@ -0,0 +1,41 @@
+﻿using BellaHair.Domain;
+using BellaHair.Domain.SharedValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaHair.Ports.PrivateCustomers
+{
+    // Mikkel Dahlmann
+
+    /// <summary>
+    /// Defines the contract for commands that operate on private customer entities.
+    /// </summary>
+    
+    public interface IPrivateCustomerCommand
+    {
+        Task CreatePrivateCustomerAsync(CreatePrivateCustomerCommand command);
+        Task DeletePrivateCustomerAsync(DeletePrivateCustomerCommand command);
+        Task UpdatePrivateCustomerAsync(UpdatePrivateCustomerCommand command);
+    }
+    
+    public record CreatePrivateCustomerCommand(
+        Name Name,
+        Address Address,
+        PhoneNumber PhoneNumber,
+        Email Email,
+        DateTime Birthday);
+
+    public class UpdatePrivateCustomerCommand(
+        Guid Id, 
+        Name Name,
+        Address Address,
+        PhoneNumber PhoneNumber,
+        Email Email,
+        DateTime Birthday);
+
+    public class DeletePrivateCustomerCommand(Guid Id);
+
+}
---
+++ BellaHair.Ports/PrivateCustomers/IPrivateCustomerQuery.cs
@@ -0,0 +1,29 @@
+﻿using BellaHair.Domain;
+using BellaHair.Domain.SharedValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaHair.Ports.PrivateCustomers
+{
+    // Mikkel Dahlmann
+
+    /// <summary>
+    /// Defines a query interface for retrieving private customer records asynchronously.
+    /// </summary>
+    
+    public interface IPrivateCustomerQuery
+    {
+        Task<List<PrivateCustomerDTO>> GetPrivateCustomers();
+    }
+
+    public record PrivateCustomerDTO(
+        Guid Id,
+        Name Name,
+        Address Address,
+        PhoneNumber PhoneNumber,
+        Email Email,
+        DateTime Birthday);
+}
