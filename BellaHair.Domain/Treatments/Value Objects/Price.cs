7e8f4c09b5d777148699be9b7da9a4310eceb19f
mikl0w <mikkelklitgaard@live.dk>
2025-11-12 09:39:34 +0100

    Created Price value object for Treaments

--- BellaHair.Domain/Treatments/Value Objects/Price.cs
+++ BellaHair.Domain/Treatments/Value Objects/Price.cs
@@ -2,6 +2,28 @@
 {
     public record Price
     {
+        public decimal Value { get; private init; }
+
+
+        protected Price() { }
+
+
+        private Price(decimal value)
+        {
+            Value = value;
+        }
+
+        public static Price FromDecimal(decimal price) => new Price(price);
+
+        public static void ValidatePrice(decimal value)
+        {
+            if (value < 0)
+                throw new PriceException("Price cannot be less than zero.");
+            if (value > 100000)
+                throw new PriceException("Price cannot exceed 100.000");
+        }
 
     }
+
+    public class PriceException(string message) : Exception(message);
 }
