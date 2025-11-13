652aa143f315b52444eca48be813c3e6ce205657
mikl0w <mikkelklitgaard@live.dk>
2025-11-13 13:36:03 +0100

    Save changes

--- BellaHair.Infrastructure/Treatments/TreatmentRepository.cs
+++ BellaHair.Infrastructure/Treatments/TreatmentRepository.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BellaHair.Domain.Treatments;
 
 namespace BellaHair.Infrastructure.Treatments
 {
-    public class TreatmentRepository
+    public class TreatmentRepository : ITreatmentRepository
     {
+        private readonly BellaHairContext _db;
+
+        public TreatmentRepository(BellaHairContext db)
+        {
+            _db = db;
+        }
+
+        async Task ITreatmentRepository.AddAsync(Treatment treatment)
+        {
+            await _db.Treatments.AddAsync(treatment);
+        }
+
+        void ITreatmentRepository.Delete(Treatment treatment)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<Treatment> ITreatmentRepository.Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task ITreatmentRepository.SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
