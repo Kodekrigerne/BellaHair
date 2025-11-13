867dc1d7ab72e023cd37936fb20c501f8d017fa0
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-13 14:28:33 +0100

    Created a LoyaltyDiscountCard component and moved discount ViewModels to their own file.

---
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/DiscountViewModels.cs
@@ -0,0 +1,6 @@
+﻿namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts
+{
+    public interface IDiscountViewModel;
+    public record LoyaltyDiscountViewModel(Guid Id, string Name, int MinimumVisits, decimal DiscountPercent) : IDiscountViewModel;
+    public record CampaignDiscountViewModel() : IDiscountViewModel;
+}
