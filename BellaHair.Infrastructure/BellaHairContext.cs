c7718f6f18a61a2f8ec770d7e3bfc85c3cc62b9c
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-13 13:42:14 +0100

    Added complexProperties for privatecustomer.

--- BellaHair.Infrastructure/BellaHairContext.cs
+++ BellaHair.Infrastructure/BellaHairContext.cs
@@ -2,6 +2,7 @@ using BellaHair.Domain.Bookings;
 using BellaHair.Domain.Discounts;
 using BellaHair.Domain.Treatments;
 using BellaHair.Domain.Employees;
+using BellaHair.Domain.PrivateCustomer;
 using Microsoft.EntityFrameworkCore;
 
 namespace BellaHair.Infrastructure
@@ -21,13 +22,17 @@ namespace BellaHair.Infrastructure
             //TPC mapping vælges så alle nedarvere af DiscountBase får deres egen tabel, og ingen tabel for baseklassen oprettes.
             modelBuilder.Entity<DiscountBase>().UseTpcMappingStrategy();
 
-            modelBuilder.Entity<Booking>().ComplexProperty(b => b.Discount);
+            modelBuilder.Entity<Booking>().ComplexProperty(b => b.Discount, b => b.IsRequired());
             modelBuilder.Entity<Treatment>().ComplexProperty(t => t.Price);
             modelBuilder.Entity<Treatment>().ComplexProperty(t => t.DurationMinutes);
             modelBuilder.Entity<Employee>().ComplexProperty(e => e.Name);
             modelBuilder.Entity<Employee>().ComplexProperty(e => e.Email);
             modelBuilder.Entity<Employee>().ComplexProperty(e => e.PhoneNumber);
             modelBuilder.Entity<Employee>().ComplexProperty(e => e.Address);
+            modelBuilder.Entity<PrivateCustomer>().ComplexProperty(p => p.Name);
+            modelBuilder.Entity<PrivateCustomer>().ComplexProperty(p => p.Email);
+            modelBuilder.Entity<PrivateCustomer>().ComplexProperty(p => p.PhoneNumber);
+            modelBuilder.Entity<PrivateCustomer>().ComplexProperty(p => p.Address);
 
             modelBuilder.Entity<LoyaltyDiscount>().ComplexProperty(l => l.DiscountPercent);
         }
