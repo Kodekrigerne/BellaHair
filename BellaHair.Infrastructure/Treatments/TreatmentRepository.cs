b9ff56ce24ed412f5a67204c1943a3e266f5d31a
mikl0w <mikkelklitgaard@live.dk>
2025-11-13 13:51:15 +0100

    TreatmentRepository done

--- BellaHair.Infrastructure/Treatments/TreatmentRepository.cs
+++ BellaHair.Infrastructure/Treatments/TreatmentRepository.cs
@@ -2,14 +2,19 @@
 
 namespace BellaHair.Infrastructure.Treatments
 {
+    // Mikkel Klitgaard
+
+    /// <summary>
+    /// Provides methods for managing treatments in the BellaHair database.
+    /// </summary>
+    /// <remarks>This repository implements the <see cref="ITreatmentRepository"/> interface and provides
+    /// functionality for adding, deleting, retrieving, and saving treatments. It interacts with the underlying database
+    /// context <see cref="BellaHairContext"/> to perform these operations.</remarks>
     public class TreatmentRepository : ITreatmentRepository
     {
         private readonly BellaHairContext _db;
 
-        public TreatmentRepository(BellaHairContext db)
-        {
-            _db = db;
-        }
+        public TreatmentRepository(BellaHairContext db) => _db = db;
 
         async Task ITreatmentRepository.AddAsync(Treatment treatment)
         {
@@ -18,17 +23,18 @@ namespace BellaHair.Infrastructure.Treatments
 
         void ITreatmentRepository.Delete(Treatment treatment)
         {
-            throw new NotImplementedException();
+            _db.Remove(treatment);
         }
 
         async Task<Treatment> ITreatmentRepository.Get(Guid id)
         {
-            throw new NotImplementedException();
+            return await _db.Treatments.FindAsync(id) ??
+                   throw new KeyNotFoundException($"Treatment with id {id} is not found.");
         }
 
         async Task ITreatmentRepository.SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            await _db.SaveChangesAsync();
         }
     }
 }
