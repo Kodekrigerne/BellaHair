d6f9c6ba19f23efb7caa1d29dec1df859acf7b1b
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-13 15:09:48 +0100

    Update RepositoryTests with new test methods
    Replaced the commented-out `Given_NewPrivateCustomer_Then_AddsPrivateCustomerToDatabase` test with an updated implementation.
    
    Added two new test methods:
    - `Given_PrivateCustomerExistsInDatabase_Then_ReturnsPrivateCustomer` to verify retrieval of an existing private customer.
    - `Given_PrivateCustomerExistsInDatabase_Then_DeletePrivateCustomerFromDatabase` to verify deletion of an existing private customer.
    
    Included the `System.Threading.Tasks` namespace to support asynchronous operations in the test methods.

--- BellaHair.Infrastructure.Tests/PrivateCustomerTests/RepositoryTests.cs
+++ BellaHair.Infrastructure.Tests/PrivateCustomerTests/RepositoryTests.cs
@@ -12,27 +12,78 @@ namespace BellaHair.Infrastructure.Tests.PrivateCustomerTests
 {
     internal sealed class RepositoryTests : InfrastructureTestBase
     {
-        //[Test]
-        //public void Given_NewPrivateCustomer_Then_AddsPrivateCustomerToDatabase()
-        //{
-        //    // Arrange
-        //    var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
+        [Test]
+        public void Given_NewPrivateCustomer_Then_AddsPrivateCustomerToDatabase()
+        {
+            // Arrange
+            var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
 
-        //    var name = Name.FromStrings("Mikkel", "Dahlmann", "Frostholm");
-        //    var address = Address.Create("Vej", "By", "1", 7100, 2);
-        //    var phoneNumber = PhoneNumber.FromString("12345678");
-        //    var email = Email.FromString("email@email.com");
-        //    var birthday = DateTime.Now.AddYears(-19);
+            var name = Name.FromStrings("Mikkel", "Dahlmann", "Frostholm");
+            var address = Address.Create("Vej", "By", "1", 7100, 2);
+            var phoneNumber = PhoneNumber.FromString("12345678");
+            var email = Email.FromString("email@email.com");
+            var birthday = DateTime.Now.AddYears(-19);
 
-        //    var customer = PrivateCustomer.Create(name, address, phoneNumber, email, birthday);
+            var customer = PrivateCustomer.Create(name, address, phoneNumber, email, birthday);
 
-        //    // Act
-        //    repo.AddAsync(customer);
-        //    repo.SaveChangesAsync();
+            // Act
+            repo.AddAsync(customer);
+            repo.SaveChangesAsync();
 
-        //    // Assert
-        //    var actualCustomer = _db.PrivateCustomers.First();
-        //    Assert.That(actualCustomer.Id, Is.EqualTo(customer.Id));
-        //}
+            // Assert
+            var actualCustomer = _db.PrivateCustomers.First();
+            Assert.That(actualCustomer.Id, Is.EqualTo(customer.Id));
+        }
+
+        [Test]
+        public void Given_PrivateCustomerExistsInDatabase_Then_ReturnsPrivateCustomer()
+        {
+            // Arrange
+            var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
+
+            var name = Name.FromStrings("Mikkel", "Dahlmann", "Frostholm");
+            var address = Address.Create("Vej", "By", "1", 7100, 2);
+            var phoneNumber = PhoneNumber.FromString("12345678");
+            var email = Email.FromString("email@email.com");
+            var birthday = DateTime.Now.AddYears(-19);
+
+            var customer = PrivateCustomer.Create(name, address, phoneNumber, email, birthday);
+
+            repo.AddAsync(customer);
+            repo.SaveChangesAsync();
+
+            // Act
+            var returnedCustomer = repo.GetAsync(customer.Id).GetAwaiter().GetResult();
+
+            // Arrange
+            Assert.That(returnedCustomer.Id, Is.EqualTo(customer.Id));
+        }
+
+        [Test]
+        public void Given_PrivateCustomerExistsInDatabase_Then_DeletePrivateCustomerFromDatabase()
+        {
+            // Arrange
+            var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
+
+            var name = Name.FromStrings("Mikkel", "Dahlmann", "Frostholm");
+            var address = Address.Create("Vej", "By", "1", 7100, 2);
+            var phoneNumber = PhoneNumber.FromString("12345678");
+            var email = Email.FromString("email@email.com");
+            var birthday = DateTime.Now.AddYears(-19);
+
+            var customer = PrivateCustomer.Create(name, address, phoneNumber, email, birthday);
+
+            repo.AddAsync(customer);
+            repo.SaveChangesAsync();
+
+            var customerToDelete = _db.PrivateCustomers.First();
+
+            // Act
+            repo.Delete(customerToDelete);
+            repo.SaveChangesAsync();
+
+            // Assert
+            Assert.That(_db.PrivateCustomers.Find(customerToDelete.Id), Is.Null);
+        }
     }
 }
