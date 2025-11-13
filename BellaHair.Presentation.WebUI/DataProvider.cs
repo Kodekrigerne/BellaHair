5447a8af771d28b40581bd6735283aeccf068a11
mikl0w <mikkelklitgaard@live.dk>
2025-11-13 14:37:14 +0100

    Added mock data to DataProvider: AddTreatment() method implemented and called in AddData().

--- BellaHair.Presentation.WebUI/DataProvider.cs
+++ BellaHair.Presentation.WebUI/DataProvider.cs
@@ -1,4 +1,7 @@
 ﻿using BellaHair.Domain.Discounts;
+using BellaHair.Domain.SharedValueObjects;
+using BellaHair.Domain.Treatments;
+using BellaHair.Domain.Treatments.ValueObjects;
 using BellaHair.Infrastructure;
 
 namespace BellaHair.Presentation.WebUI
@@ -12,6 +15,7 @@ namespace BellaHair.Presentation.WebUI
         public void AddData()
         {
             AddLoyaltyDiscounts();
+            AddTreatment();
 
             _db.SaveChangesAsync();
         }
@@ -21,6 +25,13 @@ namespace BellaHair.Presentation.WebUI
             _db.Add(LoyaltyDiscount.Create("Loyalty5", 5, DiscountPercent.FromDecimal(0.05m)));
             _db.Add(LoyaltyDiscount.Create("Loyalty10", 10, DiscountPercent.FromDecimal(0.10m)));
             _db.Add(LoyaltyDiscount.Create("Loyalty15", 15, DiscountPercent.FromDecimal(0.15m)));
+        }
+
+        private void AddTreatment()
+        {
+            _db.Add(Treatment.Create("Herreklip", Price.FromDecimal(450m), DurationMinutes.FromInt(30)));
+            _db.Add(Treatment.Create("Dameklip", Price.FromDecimal(600m), DurationMinutes.FromInt(60)));
+            _db.Add(Treatment.Create("Dame Hårfarvning", Price.FromDecimal(400m), DurationMinutes.FromInt(90)));
         }
     }
 }
