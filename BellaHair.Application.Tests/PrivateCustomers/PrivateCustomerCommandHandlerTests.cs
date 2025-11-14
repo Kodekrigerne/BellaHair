cbc3cdb830d6f0c3b733908e7608556f337d4d6c
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-14 09:54:44 +0100

    Add and update tests for PrivateCustomer operations
    - Added a new test for updating private customer data, ensuring that
      updates to customer details are correctly persisted in the database.

--- BellaHair.Application.Tests/PrivateCustomers/PrivateCustomerCommandHandlerTests.cs
+++ BellaHair.Application.Tests/PrivateCustomers/PrivateCustomerCommandHandlerTests.cs
@@ -3,6 +3,7 @@ using BellaHair.Domain;
 using BellaHair.Domain.PrivateCustomers;
 using BellaHair.Domain.SharedValueObjects;
 using BellaHair.Infrastructure.PrivateCustomers;
+using BellaHair.Ports.Discounts;
 using BellaHair.Ports.PrivateCustomers;
 using System;
 using System.Collections.Generic;
@@ -36,12 +37,51 @@ namespace BellaHair.Application.Tests.PrivateCustomers
             });
         }
 
-        //[Test]
-        //public void Given_PrivateCustomerData_Then_DeletesPrivateCustomerFromDatabase()
-        //{
-        //    // Arrange
-        //    var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
-        //    var handler = (IPrivateCustomerCommand)new PrivateCustomerCommandHandler(repo);
-        //}
+        [Test]
+        public void Given_PrivateCustomerData_Then_DeletesPrivateCustomerFromDatabase()
+        {
+            // Arrange
+            var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
+            var handler = (IPrivateCustomerCommand)new PrivateCustomerCommandHandler(repo);
+
+            var customer0 = PrivateCustomer.Create(Name.FromStrings("Mikkel", "Dahlmann"),
+                Address.Create("Gade", "By", "1", 7100), PhoneNumber.FromString("12345678"),
+                Email.FromString("email@email.com"), DateTime.Now.AddYears(-20));
+
+            _db.Add(customer0);
+            _db.SaveChanges();
+
+            var command = new DeletePrivateCustomerCommand(customer0.Id);
+
+            //Act
+            handler.DeletePrivateCustomerAsync(command);
+
+            //Assert
+            Assert.That(_db.PrivateCustomers.Any(), Is.False);
+        }
+
+        [Test]
+        public void Given_UpdatedPrivateCustomerData_Then_UpdatesPrivateCustomerInDatabase()
+        {
+            // Arrange
+            var repo = (IPrivateCustomerRepository)new PrivateCustomerRepository(_db);
+            var handler = (IPrivateCustomerCommand)new PrivateCustomerCommandHandler(repo);
+
+            var customer0 = PrivateCustomer.Create(Name.FromStrings("Mikkel", "Dahlmann"),
+                Address.Create("Gade", "By", "1", 7100), PhoneNumber.FromString("12345678"),
+                Email.FromString("email@email.com"), DateTime.Now.AddYears(-20));
+
+            _db.Add(customer0);
+            _db.SaveChanges();
+
+            var command = new UpdatePrivateCustomerCommand(customer0.Id, "Mikkel", null, "Dahlmann",
+                "Gade", "By", "1", 7100, null, "12345678", "updated@email.com", DateTime.Now.AddYears(-20));
+
+            // Act
+            handler.UpdatePrivateCustomerAsync(command);
+
+            // Assert
+            Assert.That(() => _db.PrivateCustomers.FirstOrDefault()!.Email.Value, Is.EqualTo(command.Email));
+        }
     }
 }
