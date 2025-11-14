6cf894f89b9b65cd0f806d6c4fa9810d08080312
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-14 09:29:09 +0100

    Added delete functionality to LoyaltyDiscountCard.

--- BellaHair.Presentation.WebUI/Components/Pages/Discounts/DiscountViewModels.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/DiscountViewModels.cs
@@ -1,6 +1,6 @@
 ﻿namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts
 {
-    public interface IDiscountViewModel;
+    public interface IDiscountViewModel { Guid Id { get; } }
     public record LoyaltyDiscountViewModel(Guid Id, string Name, int MinimumVisits, decimal DiscountPercent) : IDiscountViewModel;
-    public record CampaignDiscountViewModel() : IDiscountViewModel;
+    public record CampaignDiscountViewModel(Guid Id) : IDiscountViewModel;
 }
