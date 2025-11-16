cc2ecd8d37ea31a0c59437ce2b749cfca3253ae5
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-16 16:51:37 +0100

    Added comments with possible strategies for booking total and customer visits computed properties.

--- BellaHair.Domain/PrivateCustomers/PrivateCustomer.cs
+++ BellaHair.Domain/PrivateCustomers/PrivateCustomer.cs
@@ -11,23 +11,28 @@ namespace BellaHair.Domain.PrivateCustomers
 
     public class PrivateCustomer : PersonBase
     {
-        //Udregn denne ud fra mængden af fuldførte bookings, get => Bookings.Count ?? Eller som DB computed value
+        //TODO: Vælg en strategi
+        // 1. .Include(c => c.Bookings) Men loader alle bookings ind sammen med kunde,
+        //      kan være overkill hvis vi kun skal bruge count. Hvis bookings skal med uanset er det fint.
+        // 2. Flyt Visits ud af entitet, udregn den udenfor (i query) og send med i DTO (så den findes I DTO men ikke entitet)
+        // 3. Database computed value, burde være simpelt, men ingen kontrol over nutid
+        // 4. GetVisits(ICustomerVisitsCalculator _) metode
         public int Visits => Bookings.Count;
         public DateTime Birthday { get; private set; }
         private readonly List<Booking> _bookings;
-        
+
         // Den offentlige liste af bookings gøres immutable gennem casting til en IReadOnlyCollection.
         public IReadOnlyCollection<Booking> Bookings => _bookings.AsReadOnly();
 
-        #pragma warning disable CS8618
+#pragma warning disable CS8618
         private PrivateCustomer() { }
-        #pragma warning restore CS8618
+#pragma warning restore CS8618
 
 
         private PrivateCustomer(Name name, Address address, PhoneNumber phoneNumber, Email email, DateTime birthday)
         {
             ValidateBirthday(birthday);
-            
+
             Id = Guid.NewGuid();
             Name = name;
             Address = address;
