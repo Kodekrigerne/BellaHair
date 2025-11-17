0e014a3cc4a375db045e77cb3a2abb4c6a172035
mikl0w <mikkelklitgaard@live.dk>
2025-11-17 10:58:05 +0100

    Created Treatment integration tests class

---
+++ BellaHair.Application.Tests/Treatments/TreatmentCommandHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Application.Treatments;
+using BellaHair.Domain.Treatments;
+using BellaHair.Infrastructure.Treatments;
+using BellaHair.Ports.Treatments;
+
+namespace BellaHair.Application.Tests.Treatments
+{
+    internal sealed class TreatmentCommandHandlerTests : ApplicationTestBase
+    {
+
+    }
+}
