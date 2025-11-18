bb8e98b1ec017e2e6a808df8cd12b738af8821df
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-18 11:46:27 +0100

    Added eager loading to BookingOverlapChecker.

--- BellaHair.Infrastructure/Bookings/BookingOverlapChecker.cs
+++ BellaHair.Infrastructure/Bookings/BookingOverlapChecker.cs
@@ -1,27 +1,47 @@
-﻿using BellaHair.Domain.Bookings;
+﻿using BellaHair.Domain;
+using BellaHair.Domain.Bookings;
 using Microsoft.EntityFrameworkCore;
 
 namespace BellaHair.Infrastructure.Bookings
 {
+    //Dennis
+    /// <inheritdoc cref="IBookingOverlapChecker"/>
     public class BookingOverlapChecker : IBookingOverlapChecker
     {
         private readonly BellaHairContext _db;
+        private readonly ICurrentDateTimeProvider _currentDateTimeProvider;
 
-        public BookingOverlapChecker(BellaHairContext db) => _db = db;
-
-        async Task<bool> IBookingOverlapChecker.CheckOverlap(Guid employeeId, DateTime startDateTime, int durationMinutes)
+        public BookingOverlapChecker(BellaHairContext db, ICurrentDateTimeProvider currentDateTimeProvider)
         {
-            if (!await _db.Employees.AnyAsync(e => e.Id == employeeId))
-                throw new KeyNotFoundException($"No employee with ID {employeeId} exists");
+            _db = db;
+            _currentDateTimeProvider = currentDateTimeProvider;
+        }
+
+        async Task<bool> IBookingOverlapChecker.CheckOverlap(Booking booking)
+        {
+            if (booking.Employee == null) throw new InvalidOperationException($"Booking must be loaded with employee, {booking.Id} overlap check");
+            if (booking.Treatment == null) throw new InvalidOperationException($"Booking must be loaded with treatment, {booking.Id} overlap check");
+
+            // Da vi har adgang til _db kan vi loade bookingen ind med relationer hvis de mangler fra den booking der passes ind
+            if (booking.Employee == null || booking.Treatment == null)
+            {
+                booking = await _db.Bookings
+                    .Include(b => b.Employee)
+                    .Include(b => b.Treatment)
+                    .FirstOrDefaultAsync(b => b.Id == booking.Id) ?? throw new KeyNotFoundException($"Booking not found {booking.Id}");
+            }
 
             var bookings = _db.Bookings
                 .AsNoTracking()
-                .Where(b => b.Employee!.Id == employeeId)
+                .Where(b => b.StartDateTime > _currentDateTimeProvider.GetCurrentDateTime())
+                .Where(b => b.Employee!.Id == booking.Employee!.Id)
                 .Include(b => b.Employee)
                 .Include(b => b.Treatment);
 
-            if (await bookings.AnyAsync(b => startDateTime > b.StartDateTime && startDateTime < b.StartDateTime.AddMinutes(b.Treatment!.DurationMinutes.Value))) return true;
-            if (await bookings.AnyAsync(b => startDateTime.AddMinutes(durationMinutes) > b.StartDateTime && startDateTime.AddMinutes(durationMinutes) < b.StartDateTime.AddMinutes(b.Treatment!.DurationMinutes.Value))) return true;
+            // Er der nogen bookings for medarbejderen som starter før og slutter efter den nye bookings starttid?
+            if (await bookings.AnyAsync(b => booking.StartDateTime > b.StartDateTime && booking.StartDateTime < b.StartDateTime.AddMinutes(b.Treatment!.DurationMinutes.Value))) return true;
+            // Er der nogen bookings for medarbejderen som starter før og slutter efter den nye bookings sluttid?
+            if (await bookings.AnyAsync(b => booking.StartDateTime.AddMinutes(booking.Treatment!.DurationMinutes.Value) > b.StartDateTime && booking.StartDateTime.AddMinutes(booking.Treatment.DurationMinutes.Value) < b.StartDateTime.AddMinutes(b.Treatment!.DurationMinutes.Value))) return true;
 
             return false;
         }
