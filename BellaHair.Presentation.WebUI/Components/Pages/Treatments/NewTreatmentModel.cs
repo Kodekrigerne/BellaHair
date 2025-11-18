89b82d552d9553a86991bcae6737658d5b6f0462
mikl0w <mikkelklitgaard@live.dk>
2025-11-18 10:14:10 +0100

    HandleCreateTreatment method implemented OpenNewModal() method introduced

---
+++ BellaHair.Presentation.WebUI/Components/Pages/Treatments/NewTreatmentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BellaHair.Presentation.WebUI.Components.Pages.Treatments
+{
+    public class NewTreatmentModel
+    {
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int DurationMinutes { get; set; }
+    }
+}
