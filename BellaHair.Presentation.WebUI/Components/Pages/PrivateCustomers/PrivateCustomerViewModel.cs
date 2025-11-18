9f0c77596ad27a4ad490bba4b4da77df0a8b6d3c
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-18 13:56:28 +0100

    Refactored privatecustomercard into its own component.

---
+++ BellaHair.Presentation.WebUI/Components/Pages/PrivateCustomers/PrivateCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Mikkel Dahlmann
+
+namespace BellaHair.Presentation.WebUI.Components.Pages.PrivateCustomers
+{
+    public record PrivateCustomerViewModel(
+        Guid Id,
+        string FullName,
+        DateTime Birthday,
+        string Email,
+        string PhoneNumber,
+        string FullAddress,
+        int Visits);
+}
+
