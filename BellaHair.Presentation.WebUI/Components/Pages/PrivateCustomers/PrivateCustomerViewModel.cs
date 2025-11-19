399763445aca1af380558692f5f1a1dea1fca7a6
MDahlmann <dahlmann.mikkel@gmail.com>
2025-11-19 09:35:26 +0100

    Deleted excess data

--- BellaHair.Presentation.WebUI/Components/Pages/PrivateCustomers/PrivateCustomerViewModel.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/PrivateCustomers/PrivateCustomerViewModel.cs
@@ -1,23 +1,11 @@
 ﻿namespace BellaHair.Presentation.WebUI.Components.Pages.PrivateCustomers
 {
-    public record PrivateCustomerViewModel(Guid Id, string FullName, DateTime Birthday, string Email, string PhoneNumber, string FullAddress, int Visits);
-}        string Email,
-        string PhoneNumber,
-        string FullAddress,
-        int Visits);
-}
-
->>>>>>>>> Temporary merge branch 2
+    public record PrivateCustomerViewModel(
+        Guid Id,
+        string FullName,
+        DateTime Birthday,
         string Email,
         string PhoneNumber,
         string FullAddress,
         int Visits);
-}
-
->>>>>>>>> Temporary merge branch 2
-        string PhoneNumber,
-        string FullAddress,
-        int Visits);
-}
-
->>>>>>>>> Temporary merge branch 2
+};
