d8757cc1c1563b6519fe9e98278ad9684d795e77
Linnea Damgaard Jensen <ldje71303@edu.ucl.dk>
2025-11-20 10:42:08 +0100

    fixed employeeviewmodel and added delete confirm pop up

--- BellaHair.Presentation.WebUI/Components/Pages/Employees/EmployeeViewModels.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Employees/EmployeeViewModels.cs
@@ -1,6 +1,6 @@
 ﻿namespace BellaHair.Presentation.WebUI.Components.Pages.Employees
 {
-    public record EmployeeSimpleViewModel(Guid Id, string name, string email, string phoneNumber, List<string> treatmentNames);
+    public record EmployeeSimpleViewModel(Guid Id, string Name, string Email, string PhoneNumber, List<string> TreatmentNames);
     // TODO: Add full viewmodel
 
 }
