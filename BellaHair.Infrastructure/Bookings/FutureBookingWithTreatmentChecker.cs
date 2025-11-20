3104408c2ba3df73a187d6f47c859434a5de09c0
mikl0w <mikkelklitgaard@live.dk>
2025-11-20 10:48:08 +0100

    Created fields and constructor for class FutureBookingWithTreatmentChecker

--- BellaHair.Infrastructure/Bookings/FutureBookingWithTreatmentChecker.cs
+++ BellaHair.Infrastructure/Bookings/FutureBookingWithTreatmentChecker.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BellaHair.Domain;
 using BellaHair.Domain.Discounts;
+using BellaHair.Domain.Treatments;
 
 namespace BellaHair.Infrastructure.Bookings
 {
     public class FutureBookingWithTreatmentChecker : IFutureBookingWithTreatmentChecker
     {
+        private readonly ITreatmentRepository _treatmentRepo;
+        private readonly ICurrentDateTimeProvider _currentDateTimeProvider;
+
+        public FutureBookingWithTreatmentChecker(ITreatmentRepository treatmentRepo, ICurrentDateTimeProvider currentDateTimeProvider)
+        {
+            _treatmentRepo = treatmentRepo;
+            _currentDateTimeProvider = currentDateTimeProvider;
+        }
+
+        public Task<bool> CheckFutureBookings(Guid id)
+        {
+
+        }
     }
 }
