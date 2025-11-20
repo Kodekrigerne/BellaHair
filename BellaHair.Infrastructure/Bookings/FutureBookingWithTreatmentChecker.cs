fc77959bfb5a8eb7490436f8481e3ab2e83b6eb3
mikl0w <mikkelklitgaard@live.dk>
2025-11-20 10:44:47 +0100

    Created class FutureBookingWithTreatmentChecker. Added interface and class to serviceCollection IoC

---
+++ BellaHair.Infrastructure/Bookings/FutureBookingWithTreatmentChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Domain.Discounts;
+
+namespace BellaHair.Infrastructure.Bookings
+{
+    public class FutureBookingWithTreatmentChecker : IFutureBookingWithTreatmentChecker
+    {
+    }
+}
