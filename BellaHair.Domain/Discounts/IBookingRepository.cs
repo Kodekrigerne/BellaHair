6e80ea7c40c0f610bbbc25cd32dd477575de8e16
mikl0w <mikkelklitgaard@live.dk>
2025-11-20 10:10:25 +0100

    Added interface IBookingRepository for BookingRepository

---
+++ BellaHair.Domain/Discounts/IBookingRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Domain.Bookings;
+using BellaHair.Domain.Treatments;
+
+namespace BellaHair.Domain.Discounts
+{
+    public interface IBookingRepository
+    {
+        Task AddAsync(Booking booking);
+        void Delete(Booking booking);
+        Task<Booking> GetAsync(int id);
+        Task<IEnumerable<Booking>> GetAllAsync();
+        Task SaveChangesAsync();
+
+        Task<List<Booking>> GetFutureBookings();
+
+    }
+}
