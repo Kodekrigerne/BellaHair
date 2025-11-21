3c5fb533224c6998424de7312e058490dca4c3c3
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-21 12:08:13 +0100

    Fixed issues after merging.

--- BellaHair.Ports/Employees/IEmployeeQuery.cs
+++ BellaHair.Ports/Employees/IEmployeeQuery.cs
@@ -22,14 +22,6 @@ namespace BellaHair.Ports.Employees
     /// </summary>
     public record EmployeeDTOSimple(Guid Id, string Name, string PhoneNumber, string Email, List<string> TreatmentNames);
 
-/// <summary>
-/// Represents a data transfer object containing all information about an employee, including personal
-/// details, contact information, address, and associated treatments.
-/// </summary>
-public record EmployeeDTOFull(Guid Id, string FirstName, string MiddleName, string LastName, string Email, string PhoneNumber, string StreetName, string City, string StreetNumber, int ZipCode, List<TreatmentDTO> Treatments, int? Floor = null);
-public record GetEmployeeByIdQuery(Guid Id);
-public record EmployeeNameDTO(string FullName);
-public record GetEmployeesByTreatmentIdQuery(Guid TreatmentId);
     /// <summary>
     /// Represents a data transfer object containing all information about an employee, including personal
     /// details, contact information, address, and associated treatments.
@@ -38,6 +30,11 @@ public record GetEmployeesByTreatmentIdQuery(Guid TreatmentId);
 
     public record GetEmployeeByIdQuery(Guid Id);
 
+    public record EmployeeNameDTO(string FullName);
+
+    public record GetEmployeesByTreatmentIdQuery(Guid TreatmentId);
+
     public record EmployeeNameWithBookingsDTO(Guid Id, string Name, List<BookingTimesOnlyDTO> Bookings);
+
     public record BookingTimesOnlyDTO(DateTime StartDateTime, int DurationMinutes);
 }
