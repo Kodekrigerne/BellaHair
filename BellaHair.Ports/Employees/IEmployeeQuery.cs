892fe44cec1f4636b0b9e9885ea6fc3d649cd16b
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-21 12:01:14 +0100

    Added a method to get all employees with a specific treatment and with future bookings included to IEmployeeQuery.

--- BellaHair.Ports/Employees/IEmployeeQuery.cs
+++ BellaHair.Ports/Employees/IEmployeeQuery.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BellaHair.Ports.Treatments;
+﻿using BellaHair.Ports.Treatments;
 
 namespace BellaHair.Ports.Employees
 {
@@ -11,24 +6,29 @@ namespace BellaHair.Ports.Employees
     /// <summary>
     /// Defines methods for querying employee data with varying levels of detail.
     /// </summary>
-    
+
     // Linnea
     public interface IEmployeeQuery
     {
         Task<List<EmployeeDTOSimple>> GetAllEmployeesSimpleAsync();
+        Task<List<EmployeeNameWithBookingsDTO>> GetHasTreatmentAndWithFutureBookingsAsync(Guid treatmentId);
         Task<EmployeeDTOFull> GetEmployeeAsync(GetEmployeeByIdQuery query);
     }
+
+    /// <summary>
+    /// Represents a simplified data transfer object for an employee, including basic contact information and a list of
+    /// associated treatment names.
+    /// </summary>
+    public record EmployeeDTOSimple(Guid Id, string Name, string PhoneNumber, string Email, List<string> TreatmentNames);
+
+    /// <summary>
+    /// Represents a data transfer object containing all information about an employee, including personal
+    /// details, contact information, address, and associated treatments.
+    /// </summary>
+    public record EmployeeDTOFull(Guid Id, string FirstName, string MiddleName, string LastName, string Email, string PhoneNumber, string StreetName, string City, string StreetNumber, int ZipCode, List<TreatmentDTO> Treatments, int? Floor = null);
+
+    public record GetEmployeeByIdQuery(Guid Id);
+
+    public record EmployeeNameWithBookingsDTO(Guid Id, string Name, List<BookingTimesOnlyDTO> Bookings);
+    public record BookingTimesOnlyDTO(DateTime StartDateTime, int DurationMinutes);
 }
-
-/// <summary>
-/// Represents a simplified data transfer object for an employee, including basic contact information and a list of
-/// associated treatment names.
-/// </summary>
-public record EmployeeDTOSimple(Guid Id, string Name, string PhoneNumber, string Email, List<string> TreatmentNames);
-
-/// <summary>
-/// Represents a data transfer object containing all information about an employee, including personal
-/// details, contact information, address, and associated treatments.
-/// </summary>
-public record EmployeeDTOFull(Guid Id, string FirstName, string MiddleName, string LastName, string Email, string PhoneNumber, string StreetName, string City, string StreetNumber, int ZipCode, List<TreatmentDTO> Treatments, int? Floor = null);
-public record GetEmployeeByIdQuery(Guid Id);
--- BellaHair.Presentation.WebUI/Components/Pages/Employees/EmployeeViewModels.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Employees/EmployeeViewModels.cs
@@ -1,6 +1,8 @@
-﻿namespace BellaHair.Presentation.WebUI.Components.Pages.Employees
+﻿using BellaHair.Presentation.WebUI.Components.Pages.Bookings;
+
+namespace BellaHair.Presentation.WebUI.Components.Pages.Employees
 {
     public record EmployeeSimpleViewModel(Guid Id, string name, string email, string phoneNumber, List<string> treatmentNames);
-    // TODO: Add full viewmodel
 
+    public record EmployeeNameWithBookingsViewModel(Guid Id, string Name, List<BookingTimesOnlyViewModel> Bookings);
 }
