e761ae601338daa9546242391194a78519a86aed
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-11-24 13:34:54 +0100

    Added EndDateTime to booking and changed all instances where AddMinutes was used to instead use EndDateTime.

--- BellaHair.Infrastructure/Bookings/FutureBookingWithTreatmentChecker.cs
+++ BellaHair.Infrastructure/Bookings/FutureBookingWithTreatmentChecker.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BellaHair.Domain;
+﻿using BellaHair.Domain;
 using BellaHair.Domain.Bookings;
-using BellaHair.Domain.Treatments;
 using Microsoft.EntityFrameworkCore;
 
 namespace BellaHair.Infrastructure.Bookings
@@ -32,7 +26,7 @@ namespace BellaHair.Infrastructure.Bookings
             return await _db.Bookings
                 .AsNoTracking()
                 .Where(b => b.Treatment!.Id == treatmentId)
-                .AnyAsync(b => b.StartDateTime.AddMinutes(b.Treatment!.DurationMinutes.Value) > currentDateTime);
+                .AnyAsync(b => b.EndDateTime > currentDateTime);
         }
     }
 }
--- BellaHair.Infrastructure/Employees/EmployeeFutureBookingsChecker.cs
+++ BellaHair.Infrastructure/Employees/EmployeeFutureBookingsChecker.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BellaHair.Domain;
+﻿using BellaHair.Domain;
 using BellaHair.Domain.Employees;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +10,7 @@ namespace BellaHair.Infrastructure.Employees
     /// <summary>
     /// Provides functionality to determine whether an employee has any bookings scheduled for a future date and time.
     /// </summary>
-    
+
     public class EmployeeFutureBookingsChecker : IEmployeeFutureBookingsChecker
     {
         private readonly ICurrentDateTimeProvider _currentDateTimeProvider;
@@ -36,9 +30,9 @@ namespace BellaHair.Infrastructure.Employees
         /// <param name="id">The unique identifier of the employee to check for future bookings.</param>
         /// <returns>A task that represents the asynchronous operation. The task result contains <see langword="true"/> if the
         /// employee has at least one future booking with a treatment; otherwise, <see langword="false"/>.</returns>
-         async Task<bool> IEmployeeFutureBookingsChecker.EmployeeHasFutureBookings(Guid id)
+        async Task<bool> IEmployeeFutureBookingsChecker.EmployeeHasFutureBookings(Guid id)
         {
-            return (await _db.Employees.Include(e => e.Bookings.Where(b => b.Treatment!= null && b.StartDateTime.AddMinutes(b.Treatment.DurationMinutes.Value) > _currentDateTimeProvider.GetCurrentDateTime()))
+            return (await _db.Employees.AsNoTracking().Include(e => e.Bookings.Where(b => b.EndDateTime > _currentDateTimeProvider.GetCurrentDateTime()))
                 .FirstAsync(p => p.Id == id))
                 .Bookings.Any();
         }
