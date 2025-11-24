59431660da679d48b6589f50237dae95de462d60
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2025-11-24 17:06:12 +0100

    Introduced and implemented interface ICampaignDiscountCommand in Ports

---
+++ BellaHair.Ports/Discounts/ICampaignDiscountCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace BellaHair.Ports.Discounts
+{
+    public interface ICampaignDiscountCommand
+    {
+        Task CreateCampaignDiscountAsync(CreateCampaignDiscountCommand command);
+        Task DeleteCampaignDiscountAsync(DeleteCampaignDiscountCommand command);
+    }
+
+    public class CreateCampaignDiscountCommand(
+        string Name,
+        decimal Discount,
+        DateTime StartDate,
+        DateTime EndDate,
+        IEnumerable<Guid> TreatmentIds);
+
+
+    public class DeleteCampaignDiscountCommand(Guid id);
+
+
+}
