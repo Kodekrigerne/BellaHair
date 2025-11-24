6b0a2c8a143618454194c512b111bf648b0d5ae1
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2025-11-24 21:04:53 +0100

    Introduced and implemented interface ICampaignDiscountQuery in Ports layer and created CampaignDiscountDTO

---
+++ BellaHair.Ports/Discounts/ICampaignDiscountQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaHair.Ports.Discounts
+{
+    public interface ICampaignDiscountQuery
+    {
+        Task<List<CampaignDiscountDTO>> GetAllAsync();
+        Task<int> GetCountAsync();
+    }
+
+    public record CampaignDiscountDTO(
+        string Name,
+        decimal DiscountPercent,
+        DateTime StartDate,
+        DateTime EndDate,
+        IEnumerable<Guid> TreatmentIds);
+}
