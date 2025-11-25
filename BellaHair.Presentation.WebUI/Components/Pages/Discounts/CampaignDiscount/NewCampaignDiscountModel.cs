f5cfcadf5a00d685d066e663384cc01c1f566510
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2025-11-25 22:12:16 +0100

    Introduced and implemented HandleCreateCampaignDiscount and fixed a bug with Discount where you had to input a discount percent between 0-1. A new property DiscountPercentInput has been added which allow input 0-100 and converts to 0-1 when added.

--- BellaHair.Presentation.WebUI/Components/Pages/Discounts/CampaignDiscount/NewCampaignDiscountModel.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/CampaignDiscount/NewCampaignDiscountModel.cs
@@ -8,8 +8,16 @@ namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts.CampaignDiscou
         [Required(ErrorMessage = "Kampagnenavn er påkrævet.")]
         public string Name { get; set; } = "";
 
-        [Range(1, 100, ErrorMessage = "Rabatten skal være mellem 1 - 100%")]
+        // Den "ægte" værdi til backenden (0.0 - 1.0)
         public decimal DiscountPercent { get; set; }
+
+        // Wrapper property til UI (1 - 100)
+        [Range(1, 100, ErrorMessage = "Rabatten skal være mellem 1 - 100%")]
+        public int DiscountPercentInput
+        {
+            get => (int)(DiscountPercent * 100);
+            set => DiscountPercent = value / 100m;
+        }
 
         [Required(ErrorMessage = "Startdato skal angives.")]
         [FutureDate]
