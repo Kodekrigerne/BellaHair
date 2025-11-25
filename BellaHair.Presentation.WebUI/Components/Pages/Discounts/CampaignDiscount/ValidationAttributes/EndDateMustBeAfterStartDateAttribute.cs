8e7c9d1e8e4cee95e8d675da7aa796e1ccb069f0
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2025-11-25 21:44:20 +0100

    Added Validation attributes for input fields

---
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/CampaignDiscount/ValidationAttributes/EndDateMustBeAfterStartDateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts.CampaignDiscount.ValidationAttributes
+{
+    public class EndDateMustBeAfterStartDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? input, ValidationContext validationContext)
+        {
+            //Vi fortæller at objektet er af typen NewCampaignDiscountModel, så vi let kan tilgå StartDate og EndDate
+            var model = (NewCampaignDiscountModel)validationContext.ObjectInstance;
+
+            if (model.EndDate < model.StartDate)
+            {
+                return new ValidationResult("Slutdato skal være efter startdato.", [validationContext.MemberName!]);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/CampaignDiscount/ValidationAttributes/FutureDateAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts.CampaignDiscount.ValidationAttributes
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? input, ValidationContext validationContext)
+        {
+            if (input is DateTime dateTimeValue)
+            {
+                if (dateTimeValue < DateTime.Now)
+                {
+                    return new ValidationResult("Datoen skal være i fremtiden.", [validationContext.MemberName!]);
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
