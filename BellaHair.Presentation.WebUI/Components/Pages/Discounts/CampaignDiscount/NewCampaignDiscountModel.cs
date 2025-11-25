fa20ca7bc3d7558b2c1b3f963f44ac01a9d50cdf
Mikkel Benjamin Klitgaard <mbkl71311@edu.ucl.dk>
2025-11-25 21:46:04 +0100

    Introduced and implemented NewCampaignDiscountModel class for creating CampaignDiscounts, with validation

---
+++ BellaHair.Presentation.WebUI/Components/Pages/Discounts/CampaignDiscount/NewCampaignDiscountModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BellaHair.Presentation.WebUI.Components.Pages.Discounts.CampaignDiscount.ValidationAttributes;
+
+namespace BellaHair.Presentation.WebUI.Components.Pages.Discounts.CampaignDiscount
+{
+    public class NewCampaignDiscountModel
+    {
+        [Required(ErrorMessage = "Kampagnenavn er påkrævet.")]
+        public string Name { get; set; } = "";
+
+        [Range(1, 100, ErrorMessage = "Rabatten skal være mellem 1 - 100%")]
+        public decimal DiscountPercent { get; set; }
+
+        [Required(ErrorMessage = "Startdato skal angives.")]
+        [FutureDate]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Slutdato skal angives.")]
+        [FutureDate]
+        [EndDateMustBeAfterStartDate]
+        public DateTime EndDate { get; set; }
+
+        public IEnumerable<Guid> TreatmentIds { get; set; } = [];
+    }
+}
