459f854b82306235bb7773cb7dcafc3ee1ba2f3b
mikl0w <mikkelklitgaard@live.dk>
2025-11-27 14:23:00 +0100

    Introduced and implemented entity BirthdayDiscount.cs

---
+++ BellaHair.Domain/Discounts/BirthdayDiscount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Domain.Bookings;
+using BellaHair.Domain.PrivateCustomers;
+using BellaHair.Domain.SharedValueObjects;
+
+namespace BellaHair.Domain.Discounts
+{
+    public class BirthdayDiscount : DiscountBase
+    {
+        public string Name { get; private set; }
+        public DiscountPercent DiscountPercent { get; private set; }
+
+
+#pragma warning disable CS8618
+        private BirthdayDiscount() {}
+#pragma warning restore CS8618
+
+        private BirthdayDiscount(string name, DiscountPercent discountPercent)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            DiscountPercent = discountPercent;
+        }
+
+        public static BirthdayDiscount Create(string discountName, DiscountPercent discountPercent) 
+            => new(discountName, discountPercent);
+
+
+        public override BookingDiscount CalculateBookingDiscount(Booking booking)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BirthdayDiscountException(string message) : DomainException(message);
+}
