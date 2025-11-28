82cccddc48227800a1495e36acedf29f4ecf4708
mikl0w <mikkelklitgaard@live.dk>
2025-11-28 10:37:13 +0100

    Added comments and author to enum DiscountType and added important rule for adding new discounts to the enum

--- BellaHair.Domain/Discounts/DiscountType.cs
+++ BellaHair.Domain/Discounts/DiscountType.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace BellaHair.Domain.Discounts
 {
+    // Mikkel Klitgaard
+    /// <summary>
+    /// Specifies the types of discounts that can be applied to a transaction.
+    /// </summary>
+    /// <remarks>Use this enumeration to indicate the reason or category for a discount.</remarks>
+
+
+    // !! IMPORTANT !!
+    //Når du opretter en ny rabat-type, så HUSK også at tilføje denne til DiscountTypeDTO i BellaHair.Ports
+    //Ændre ikke på rækkefølgen, da vi caster DiscountTypeDTO til DiscountType
     public enum DiscountType
     {
-        LoyaltyDiscount,
-        CampaignDiscount,
-        BirthdayDiscount
+        LoyaltyDiscount = 0,
+        CampaignDiscount = 1,
+        BirthdayDiscount = 2
     }
 }
