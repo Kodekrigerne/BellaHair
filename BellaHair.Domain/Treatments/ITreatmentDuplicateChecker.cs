5db6ae380ce0b9823d78cc24b164cd4412e7ad7e
mikl0w <mikkelklitgaard@live.dk>
2025-12-02 14:04:24 +0100

    Introduced and implemented duplicate checker for treatments

---
+++ BellaHair.Domain/Treatments/ITreatmentDuplicateChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellaHair.Domain.Treatments
+{
+    // Mikkel Klitgaard
+    /// <summary>
+    /// Defines a contract for checking whether a treatment with the specified name and duration is considered a
+    /// duplicate.
+    /// </summary>
+
+    public interface ITreatmentDuplicateChecker
+    {
+        Task<bool> IsDuplicateAsync(string name, int duration);
+    }
+    public class TreatmentDuplicateException(string message) : DomainException(message);
+}
---
+++ BellaHair.Infrastructure/Treatments/TreatmentDuplicateChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BellaHair.Domain.Treatments;
+using Microsoft.EntityFrameworkCore;
+
+namespace BellaHair.Infrastructure.Treatments
+{
+    public class TreatmentDuplicateChecker : ITreatmentDuplicateChecker
+    {
+        private readonly BellaHairContext _db;
+
+        public TreatmentDuplicateChecker(BellaHairContext db)
+            => _db = db;
+
+        public async Task<bool> IsDuplicateAsync(string name, int duration)
+        {
+            return await _db.Treatments
+                .AsNoTracking()
+                .AnyAsync(t => t.Name.ToLower() == name.ToLower()
+                               && t.DurationMinutes.Value == duration);
+        }
+    }
+}
