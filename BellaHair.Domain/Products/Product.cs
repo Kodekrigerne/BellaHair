ce078ea92f84b9e1ac628e8b0fc60ac67873fced
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-02 11:51:09 +0100

    Created a Product entity.

---
+++ BellaHair.Domain/Products/Product.cs
@@ -0,0 +1,18 @@
+﻿using BellaHair.Domain.SharedValueObjects;
+
+namespace BellaHair.Domain.Products
+{
+    public class Product : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Price Price { get; private set; }
+
+        public Product(string name, string description, Price price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
