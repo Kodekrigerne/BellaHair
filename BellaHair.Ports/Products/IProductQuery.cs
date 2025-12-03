a7cdb4fd2c0573ffcbca29edb8db228664618752
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-03 09:09:38 +0100

    Added product lines to GetBookingWithRelations in IBookingQuery.

---
+++ BellaHair.Ports/Products/IProductQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace BellaHair.Ports.Products
+{
+    public interface IProductQuery
+    {
+
+    }
+
+    public record ProductDTO(Guid Id, string Name, string Description, decimal Price);
+}
