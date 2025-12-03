c4d6e4ec11bc662a1b7291a76e617109680ee694
MDahlmann <dahlmann.mikkel@gmail.com>
2025-12-03 13:57:48 +0100

    Save

--- BellaHair.Presentation.WebUI/Components/Pages/Products/CreateProductComponents/NewProductModel.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Products/CreateProductComponents/NewProductModel.cs
@@ -14,7 +14,7 @@ namespace BellaHair.Presentation.WebUI.Components.Pages.Products.CreateProductCo
 
         [Required(ErrorMessage = "Navn er påkrævet")]
         [StringLength(100, ErrorMessage = "Navn kan ikke være længere end 100 tegn")]
-        public string Name { get => _name; set => _name = FormatName(value.Trim()); }
+        public string Name { get => _name; set => _name = value.Trim(); }
 
         [Required(ErrorMessage = "Beskrivelse er påkrævet")]
         [StringLength(500, ErrorMessage = "Beskrivelse kan ikke være længere end 500 tegn")]
@@ -23,16 +23,5 @@ namespace BellaHair.Presentation.WebUI.Components.Pages.Products.CreateProductCo
         [Required(ErrorMessage = "Pris er påkrævet")]
         [Range(0.01, 100000.00, ErrorMessage = "Prisen skal være mellem 0,01 og 100.000,00 kr.")]
         public decimal Price { get => _price; set => _price = value; }
-
-
-        private string FormatName(string? input)
-        {
-            if (string.IsNullOrEmpty(input)) return input;
-
-            string firstChar = input[0].ToString().ToUpperInvariant();
-            string restOfString = input.Substring(1).ToLowerInvariant();
-
-            return firstChar + restOfString;
-        }
     }
 }
