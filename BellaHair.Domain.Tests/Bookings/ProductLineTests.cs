99c7d0805117a0dd454d78208f7c0b4467cef8a0
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-03 09:36:13 +0100

    Wrote unit tests for ProductLine.

---
+++ BellaHair.Domain.Tests/Bookings/ProductLineTests.cs
@@ -0,0 +1,36 @@
+﻿using BellaHair.Domain.Bookings;
+using BellaHair.Domain.Products;
+using FixtureBuilder;
+
+namespace BellaHair.Domain.Tests.Bookings
+{
+    public sealed class ProductLineTests
+    {
+        [Test]
+        public void Create_Given_ValidParameters_Then_CreatesProductLine()
+        {
+            var amount = 3;
+            var name = "";
+            var description = "";
+            var price = 200m;
+
+            //Arrange
+            var quantity = Fixture.New<Quantity>().With(q => q.Value, amount).Build();
+            var product = Fixture.New<Product>()
+                .With(p => p.Name, name)
+                .With(p => p.Description, description)
+                .With(p => p.Price.Value, price)
+                .Build();
+
+            //Act
+            var productLine = ProductLine.Create(quantity, product);
+
+            //Assert
+            Assert.That(productLine.Quantity.Value, Is.EqualTo(amount));
+            Assert.That(productLine.Product.Name, Is.EqualTo(name));
+            Assert.That(productLine.Product.Description, Is.EqualTo(description));
+            Assert.That(productLine.Product.Price.Value, Is.EqualTo(price));
+            Assert.That(productLine.Id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
