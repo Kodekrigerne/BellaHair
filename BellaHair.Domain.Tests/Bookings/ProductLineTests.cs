2735022cf095f0c828ac8e29d34c82c366debb1c
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-03 09:40:38 +0100

    Wrote unit tests for ProductLineSnapshot.

--- BellaHair.Domain.Tests/Bookings/ProductLineTests.cs
+++ BellaHair.Domain.Tests/Bookings/ProductLineTests.cs
@@ -9,12 +9,12 @@ namespace BellaHair.Domain.Tests.Bookings
         [Test]
         public void Create_Given_ValidParameters_Then_CreatesProductLine()
         {
+            //Arrange
             var amount = 3;
             var name = "";
             var description = "";
             var price = 200m;
 
-            //Arrange
             var quantity = Fixture.New<Quantity>().With(q => q.Value, amount).Build();
             var product = Fixture.New<Product>()
                 .With(p => p.Name, name)
@@ -25,12 +25,15 @@ namespace BellaHair.Domain.Tests.Bookings
             //Act
             var productLine = ProductLine.Create(quantity, product);
 
-            //Assert
-            Assert.That(productLine.Quantity.Value, Is.EqualTo(amount));
-            Assert.That(productLine.Product.Name, Is.EqualTo(name));
-            Assert.That(productLine.Product.Description, Is.EqualTo(description));
-            Assert.That(productLine.Product.Price.Value, Is.EqualTo(price));
-            Assert.That(productLine.Id, Is.Not.EqualTo(Guid.Empty));
+            using (Assert.EnterMultipleScope())
+            {
+                //Assert
+                Assert.That(productLine.Quantity.Value, Is.EqualTo(amount));
+                Assert.That(productLine.Product.Name, Is.EqualTo(name));
+                Assert.That(productLine.Product.Description, Is.EqualTo(description));
+                Assert.That(productLine.Product.Price.Value, Is.EqualTo(price));
+                Assert.That(productLine.Id, Is.Not.EqualTo(Guid.Empty));
+            }
         }
     }
 }
