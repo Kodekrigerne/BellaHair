84d8fe8cfe511487cbad4363650d1862dcd33817
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-03 09:16:12 +0100

    Created a QueryHandler for Products with a GetAll method.

--- BellaHair.Ports/Products/IProductQuery.cs
+++ BellaHair.Ports/Products/IProductQuery.cs
@@ -2,7 +2,7 @@
 {
     public interface IProductQuery
     {
-
+        Task<IEnumerable<ProductDTO>> GetAllAsync();
     }
 
     public record ProductDTO(Guid Id, string Name, string Description, decimal Price);
