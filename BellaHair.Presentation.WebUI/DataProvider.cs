7db6b8d454067d6968d6462314a967baebeee8b7
linneadj <linneadgaardj@gmail.com>
2025-12-03 14:58:47 +0100

    added generatprs

--- BellaHair.Presentation.WebUI/DataProvider.cs
+++ BellaHair.Presentation.WebUI/DataProvider.cs
@@ -7,7 +7,10 @@ using BellaHair.Domain.SharedValueObjects;
 using BellaHair.Domain.Treatments;
 using BellaHair.Domain.Treatments.ValueObjects;
 using BellaHair.Infrastructure;
+using Bogus;
+using Bogus.Extensions.Denmark;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace BellaHair.Presentation.WebUI
 {
@@ -33,6 +36,8 @@ namespace BellaHair.Presentation.WebUI
             AddData();
         }
 
+        private readonly IOptions<OpeningTimesSettings> OpeningTimes;
+
         // --- 1. Treatment Fields ---
         private Treatment _herreklipUdenVaskFøn;
         private Treatment _herreklipMedVaskFøn;
@@ -42,7 +47,6 @@ namespace BellaHair.Presentation.WebUI
         private Treatment _storKlipning;
         private Treatment _luksusKur;
         private Treatment _retFarveBryn;
-        private Treatment _farveVipper;
         private Treatment _ordneBrynVipper;
         private Treatment _herreKlipPermanent;
         private Treatment _helfarveHalvkortHårUdenKlip;
@@ -65,6 +69,7 @@ namespace BellaHair.Presentation.WebUI
         private Treatment _hårOpsætningElegance;
         private Treatment _hårOpsætningKompleks;
 
+        List<Treatment> _treatments;
 
         // Employee fields
         private Employee _idaChristensen;
@@ -75,13 +80,14 @@ namespace BellaHair.Presentation.WebUI
         private Employee _madsKnudsen;
         private Employee _annaPetersen;
 
+        List<Employee> _employees = [];
+
         public void AddData()
         {
             AddLoyaltyDiscounts();
             AddTreatment();
-            AddPrivateCustomers();
             AddEmployees();
-            AddBookings();
+            AddPrivateCustomersAndBookingsUsingBogus();
             AddCampaignDiscounts();
             AddBirthdayDiscounts();
 
@@ -132,7 +138,6 @@ namespace BellaHair.Presentation.WebUI
                     _hårOpsætningElegance.Id,
                     _hårOpsætningKompleks.Id,
                     _ordneBrynVipper.Id,
-                    _farveVipper.Id,                   
                     _damefrisureBlæs.Id
                 }));
 
@@ -182,7 +187,6 @@ namespace BellaHair.Presentation.WebUI
                 new List<Guid> {
                     _luksusKur.Id,                     
                     _retFarveBryn.Id,
-                    _farveVipper.Id
                 }));
 
             // Langt Hår Helfarve Tilbud
@@ -232,8 +236,10 @@ namespace BellaHair.Presentation.WebUI
                     _luksusKur
                 }
             );
+
              
             _db.Employees.Add(_idaChristensen);
+            _employees.Add(_idaChristensen);
 
 
             // --- Jonas Hansen: Herreklip & Kort Hår Opsætning ---
@@ -254,7 +260,7 @@ namespace BellaHair.Presentation.WebUI
             );
 
             _db.Employees.Add(_jonasHansen);
-
+            _employees.Add(_jonasHansen);
 
             // --- Sofie Nielsen: Komplekse Opsætninger & Balayage Specialist ---
             _sofieNielsen = Employee.Create(
@@ -279,6 +285,7 @@ namespace BellaHair.Presentation.WebUI
             );
 
             _db.Employees.Add(_sofieNielsen);
+            _employees.Add(_sofieNielsen);
 
 
             // --- Lars Mikkelsen: Permanent & Detaljeklip ---
@@ -304,6 +311,7 @@ namespace BellaHair.Presentation.WebUI
             );
             
                 _db.Employees.Add(_larsMikkelsen);
+                _employees.Add(_larsMikkelsen);
 
 
             // --- Signe Jørgensen: Bryn/Vipper & Dameklip ---
@@ -311,7 +319,7 @@ namespace BellaHair.Presentation.WebUI
                 Name.FromStrings("Signe", "Jørgensen", "Aia"),
                 Email.FromString("signe@bellahairfrisor.dk"),
                 PhoneNumber.FromString("70807890"),
-                Address.Create("Gl. Landevej", "Børkop", "22", 7080, 2),
+                Address.Create("Gl Landevej", "Børkop", "22", 7080, 2),
                 new List<Treatment>
                 {
                     // Signe's ekspertise: Tillægsbehandlinger og grundlæggende styling
@@ -327,6 +335,7 @@ namespace BellaHair.Presentation.WebUI
             );
 
                 _db.Employees.Add(_signeJørgensen);
+                _employees.Add(_signeJørgensen);
 
 
             // --- Mads Knudsen: Permanent, Farve & Herreklip (All-rounder) ---
@@ -357,6 +366,7 @@ namespace BellaHair.Presentation.WebUI
             );
 
             _db.Employees.Add(_madsKnudsen);
+            _employees.Add(_madsKnudsen);
 
 
             // --- Anna Petersen: Balayage & Langt Hår Specialisten ---
@@ -380,18 +390,19 @@ namespace BellaHair.Presentation.WebUI
                 }
             );
             _db.Employees.Add(_annaPetersen);
+            _employees.Add(_annaPetersen);
 
         }
 
         private void AddTreatment()
         {
-            _herreklipMedVaskFøn = Treatment.Create("Herreklip inkl. vask/føn", Price.FromDecimal(350m), DurationMinutes.FromInt(30));
+            _herreklipMedVaskFøn = Treatment.Create("Herreklip inkl vask og føn", Price.FromDecimal(350m), DurationMinutes.FromInt(30));
             _herreklipUdenVaskFøn = Treatment.Create("Herreklip", Price.FromDecimal(300m), DurationMinutes.FromInt(25));
-            _damefrisureInklVaskFøn = Treatment.Create("Damefrisure inkl. vask/føn", Price.FromDecimal(445m), DurationMinutes.FromInt(40));
-            _damefrisureBlæs = Treatment.Create("Damefrisure/blæs", Price.FromDecimal(400m), DurationMinutes.FromInt(45));
+            _damefrisureInklVaskFøn = Treatment.Create("Damefrisure inkl vask og føn", Price.FromDecimal(445m), DurationMinutes.FromInt(40));
+            _damefrisureBlæs = Treatment.Create("Damefrisure og blæs", Price.FromDecimal(400m), DurationMinutes.FromInt(45));
             _lilleTilretning = Treatment.Create("Lille tilretning ved øre og nakke", Price.FromDecimal(200m), DurationMinutes.FromInt(15));
             _storKlipning = Treatment.Create("Stor klipning", Price.FromDecimal(495m), DurationMinutes.FromInt(60));
-            _retFarveBryn = Treatment.Create("Ret/farve bryn", Price.FromDecimal(110m), DurationMinutes.FromInt(15));
+            _retFarveBryn = Treatment.Create("Ret og farve bryn", Price.FromDecimal(110m), DurationMinutes.FromInt(15));
             _ordneBrynVipper = Treatment.Create("Ordnet bryn og vipper", Price.FromDecimal(330m), DurationMinutes.FromInt(35));
             _herreKlipPermanent = Treatment.Create("Herreklip med permanent", Price.FromDecimal(875m), DurationMinutes.FromInt(90));
             _helfarveHalvkortHårUdenKlip = Treatment.Create("Helfarve halvkort hår uden klip", Price.FromDecimal(575m), DurationMinutes.FromInt(120));
@@ -410,9 +421,9 @@ namespace BellaHair.Presentation.WebUI
             _staniolStriberILangtHårMedKlip = Treatment.Create("Staniol striber i langt hår med klip", Price.FromDecimal(500m), DurationMinutes.FromInt(150));
             _balayageUdenKlip = Treatment.Create("Balayage uden klip", Price.FromDecimal(950m), DurationMinutes.FromInt(190));
             _balayageMedKlip = Treatment.Create("Balyage med klip", Price.FromDecimal(1300m), DurationMinutes.FromInt(180));
-            _hårOpsætningStruktur = Treatment.Create("Håropsætning - Struktur", Price.FromDecimal(250m), DurationMinutes.FromInt(30));
-            _hårOpsætningElegance = Treatment.Create("Håropsætning - Elegance", Price.FromDecimal(450m), DurationMinutes.FromInt(60));
-            _hårOpsætningKompleks = Treatment.Create("Håropsætning - Kompleks", Price.FromDecimal(800m), DurationMinutes.FromInt(120));
+            _hårOpsætningStruktur = Treatment.Create("Håropsætning Struktur", Price.FromDecimal(250m), DurationMinutes.FromInt(30));
+            _hårOpsætningElegance = Treatment.Create("Håropsætning Elegance", Price.FromDecimal(450m), DurationMinutes.FromInt(60));
+            _hårOpsætningKompleks = Treatment.Create("Håropsætning Kompleks", Price.FromDecimal(800m), DurationMinutes.FromInt(120));
             _luksusKur = Treatment.Create("Luksuskur til hår", Price.FromDecimal(1100m), DurationMinutes.FromInt(120));
 
             _db.Add(_herreklipMedVaskFøn);
@@ -436,18 +447,98 @@ namespace BellaHair.Presentation.WebUI
             _db.Add(_hårOpsætningStruktur); // Håropsætning Kort
             _db.Add(_hårOpsætningElegance); // Håropsætning Mellem
             _db.Add(_hårOpsætningKompleks); // Håropsætning Lang
+
+            _treatments = new List<Treatment>
+                {
+                    _herreklipUdenVaskFøn,
+                    _herreklipMedVaskFøn,
+                    _damefrisureInklVaskFøn,
+                    _damefrisureBlæs,
+                    _lilleTilretning,
+                    _storKlipning,
+                    _luksusKur,
+                    _retFarveBryn,
+                    _ordneBrynVipper,
+                    _herreKlipPermanent,
+                    _permanentHalvkortHårMedKlip,
+                    _permanentHalvkortHårUdenKlip,
+                    _permanentLangtHårUdenKlip,
+                    _permanentLangtHårMedKlip,
+                    _helfarveHalvkortHårUdenKlip,
+                    _helfarveHalvKortHårMedKlip,
+                    _helfarveLangtHårUdenKlip,
+                    _helfarveLangtHårMedKlip,
+                    _hætteStriberIHalvkortHårMedKlip,
+                    _hætteStriberILangtHårMedKlip,
+                    _staniolStriberIHalvkortHårUdenKlip,
+                    _staniolStriberIHalvkortHårMedKlip,
+                    _staniolStriberILangtHårUdenKlip,
+                    _staniolStriberILangtHårMedKlip,
+                    _balayageUdenKlip,
+                    _balayageMedKlip,
+                    _hårOpsætningStruktur,
+                    _hårOpsætningElegance,
+                    _hårOpsætningKompleks
+                };
         }
 
-        private void AddPrivateCustomers()
+        private void AddPrivateCustomersAndBookingsUsingBogus()
         {
-          
-        }
 
-        private void AddBookings()
-        {
             var now = _currentDateTimeProvider.GetCurrentDateTime();
 
-           
+            var customers = new List<PrivateCustomer>();
+
+            for (int i = 0; i < 250; i++)
+            {
+
+            var danishFaker = new Faker<PrivateCustomer>("nb_NO")
+                .CustomInstantiator(f => PrivateCustomer.Create(
+                                                                Name.FromStrings(f.Name.FirstName(), f.Name.LastName()),
+                                                                Address.Create(f.Address.StreetName().Replace(".", ""), f.Address.City(), f.Random.Int(min: 1, max: 200).ToString(), f.Random.Int(min: 1000, max: 9990)),
+                                                                PhoneNumber.FromString(f.Phone.PhoneNumber("########")),
+                                                                Email.FromString(f.Internet.Email()),
+                                                                f.Date.Between(now.AddYears(-18), now.AddYears(-110)),
+                                                                _currentDateTimeProvider
+                                                                ));
+            var customer = danishFaker.Generate();
+
+
+                customers.Add(customer);
+            _db.Add(customer);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+
+                    Random random = new Random();
+                    var employee = _employees[random.Next(1, 6)];
+
+                    var bookingFaker = new Faker<Booking>("nb_NO")
+                        .CustomInstantiator(f => Booking.Create(
+                                                        f.PickRandom<PrivateCustomer>(customers),
+                                                        employee,
+                                                        f.PickRandom<Treatment>(employee.Treatments),
+                                                    new DateTime(
+                                                            f.Date.Soon(60, DateTime.Now).Date.Year,
+                                                            f.Date.Soon(60, DateTime.Now).Date.Month,
+                                                            f.Date.Soon(60, DateTime.Now).Date.Day,
+                                                            f.Random.Int(10, 17),
+                                                            f.PickRandom(new[] { 0, 30 }), // Use PickRandom with an inline array
+                                                            0),
+                                                    _currentDateTimeProvider));
+                    var booking = bookingFaker.Generate();
+                    
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+
         }
 
         // Bruges da Bookings skal have en ICurrentDateTimeProvider som giver deres CreatedDate som skal være i fortiden i forhold til StartTime.
