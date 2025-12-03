1f6463c92bf94a1c074abeb9ccdc41c54152baad
MDahlmann <dahlmann.mikkel@gmail.com>
2025-12-03 12:02:38 +0100

    Add tests for product command handler functionality
    Introduced `ProductCommandHandlerTests` to validate the creation
    and addition of products to the database. Added a test method
    `CreateProduct_Given_ValidProductData_Then_CreatesAndAddsProductToDatabase`
    to ensure correct behavior when valid product data is provided.
    
    Utilized dependency injection to retrieve `IProductCommand` for
    handling product creation. Added assertions to verify product
    properties in the database. Included necessary `using` directives
    for dependencies.

---
+++ BellaHair.Application.Tests/Products/ProductCommandHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using BellaHair.Ports.Products;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BellaHair.Application.Tests.Products
+{
+    internal sealed class ProductCommandHandlerTests : ApplicationTestBase
+    {
+        [Test]
+        public void CreateProduct_Given_ValidProductData_Then_CreatesAndAddsProductToDatabase()
+        {
+            // Arrange
+            var handler = ServiceProvider.GetRequiredService<IProductCommand>();
+            var name = "Shampoo";
+            var description = "Skælshampoo";
+            var price = 39.99m;
+            var command = new CreateProductCommand(name, description, price);
+
+            // Act
+            handler.CreateProductAsync(command).GetAwaiter().GetResult();
+
+            // Assert
+            var productInDb = _db.Products.FirstOrDefault();
+            Assert.Multiple(() =>
+            {
+                Assert.That(productInDb.Name, Is.EqualTo(name));
+                Assert.That(productInDb.Description, Is.EqualTo(description));
+                Assert.That(productInDb.Price.Value, Is.EqualTo(price));
+            });
+        }
+    }
+}
