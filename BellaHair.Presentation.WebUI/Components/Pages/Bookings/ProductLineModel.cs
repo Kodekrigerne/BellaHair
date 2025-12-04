26ff0ed8305288668b86a0ba6c5e679135d0468c
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-04 17:30:19 +0100

    Added a button on BookingForm to show and pick products.

--- BellaHair.Presentation.WebUI/Components/Pages/Bookings/ProductLineModel.cs
+++ BellaHair.Presentation.WebUI/Components/Pages/Bookings/ProductLineModel.cs
@@ -4,12 +4,14 @@
     {
         public Guid ProductId { get; private init; }
         public string ProductName { get; private init; }
+        public decimal Price { get; private init; }
         public int Quantity { get; private set; }
 
-        public ProductLineModel(Guid productId, string productName, int quantity)
+        public ProductLineModel(Guid productId, string productName, decimal price, int quantity)
         {
             ProductId = productId;
             ProductName = productName;
+            Price = price;
             Quantity = quantity;
         }
 
