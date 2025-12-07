3d20a33fc6db6e7814af55ec5952a59d63353c07
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-12-07 11:53:04 +0100

    Moved scroll to bottom script into wwwroot.

---
+++ BellaHair.Presentation.WebUI/wwwroot/scripts/ScrollToBottom.js
@@ -0,0 +1,7 @@
+﻿function attachDotNetScrollHandler(element, dotnetHelper) {
+	element.addEventListener('scroll', function () {
+		if (element.scrollTop + element.clientHeight >= element.scrollHeight - 10) {
+			dotnetHelper.invokeMethodAsync('OnScrollToBottom');
+		}
+	});
+};
