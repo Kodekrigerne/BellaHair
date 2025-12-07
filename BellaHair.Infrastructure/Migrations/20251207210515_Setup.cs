dd5f65acacf27eec00c5c21def4a837d005f4114
MDahlmann <dahlmann.mikkel@gmail.com>
2025-12-07 22:30:37 +0100

    Added MS SQL integration

---
+++ BellaHair.Infrastructure/Migrations/20251207210515_Setup.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BellaHair.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Setup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BirthdayDiscount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountPercent_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BirthdayDiscount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CampaignDiscount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountPercent_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TreatmentIds = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CampaignDiscount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_Floor = table.Column<int>(type: "int", nullable: true),
+                    Address_FullAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_StreetName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_StreetNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_ZipCode = table.Column<int>(type: "int", nullable: false),
+                    Email_Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber_Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoyaltyDiscount",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MinimumVisits = table.Column<int>(type: "int", nullable: false),
+                    TreatmentDiscountPercent_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductDiscountPercent_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoyaltyDiscount", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PrivateCustomers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BirthdayDiscountUsedYears = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_Floor = table.Column<int>(type: "int", nullable: true),
+                    Address_FullAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_StreetName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_StreetNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_ZipCode = table.Column<int>(type: "int", nullable: false),
+                    Email_Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber_Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrivateCustomers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DurationMinutes_Value = table.Column<int>(type: "int", nullable: false),
+                    Price_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CustomerSnapshot_CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CustomerSnapshot_FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerSnapshot_Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerSnapshot_PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerSnapshot_FullAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerSnapshot_Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    EmployeeSnapshot_EmployeeId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    EmployeeSnapshot_FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeSnapshot_Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeSnapshot_PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeSnapshot_FullAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TreatmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    TreatmentSnapshot_TreatmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    TreatmentSnapshot_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TreatmentSnapshot_Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    TreatmentSnapshot_DurationMinutes = table.Column<int>(type: "int", nullable: true),
+                    Discount_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Discount_Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Discount_DiscountActive = table.Column<bool>(type: "bit", nullable: true),
+                    Discount_Type = table.Column<int>(type: "int", nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PaidDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false),
+                    TotalBase = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    TotalWithDiscount = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Bookings_PrivateCustomers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "PrivateCustomers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Treatments_TreatmentId",
+                        column: x => x.TreatmentId,
+                        principalTable: "Treatments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeTreatment",
+                columns: table => new
+                {
+                    EmployeesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TreatmentsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeTreatment", x => new { x.EmployeesId, x.TreatmentsId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeTreatment_Employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeTreatment_Treatments_TreatmentsId",
+                        column: x => x.TreatmentsId,
+                        principalTable: "Treatments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    InvoicePdf = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    BookingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductLine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity_Value = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductLine", x => new { x.BookingId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ProductLine_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductLine_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductLineSnapshot",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    BookingId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductLineSnapshot", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductLineSnapshot_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_CustomerId",
+                table: "Bookings",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_EmployeeId",
+                table: "Bookings",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_TreatmentId",
+                table: "Bookings",
+                column: "TreatmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeTreatment_TreatmentsId",
+                table: "EmployeeTreatment",
+                column: "TreatmentsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_BookingId",
+                table: "Invoices",
+                column: "BookingId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductLine_ProductId",
+                table: "ProductLine",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductLineSnapshot_BookingId",
+                table: "ProductLineSnapshot",
+                column: "BookingId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BirthdayDiscount");
+
+            migrationBuilder.DropTable(
+                name: "CampaignDiscount");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeTreatment");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "LoyaltyDiscount");
+
+            migrationBuilder.DropTable(
+                name: "ProductLine");
+
+            migrationBuilder.DropTable(
+                name: "ProductLineSnapshot");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "PrivateCustomers");
+
+            migrationBuilder.DropTable(
+                name: "Treatments");
+        }
+    }
+}
